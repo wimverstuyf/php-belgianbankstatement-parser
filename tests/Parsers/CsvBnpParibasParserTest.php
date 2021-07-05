db8eed177e33a65b6dc63570073ffa77c4bee3f5
Wim Verstuyf <wim.verstuyf@eenvoudigfactureren.be>
2021-07-05 15:00:31 +0200

    fix test

--- tests/Parsers/CsvBnpParibasParserTest.php
+++ tests/Parsers/CsvBnpParibasParserTest.php
@@ -40,12 +40,11 @@ class CsvBnpParibasParserTest extends \PHPUnit\Framework\TestCase
 		$this->assertEquals("NAAM VAN DE KLANT BE15 3215 3215 2185  BIC A32DEDF5 MEDEDELING : dit is een custom message     VALUTADATUM : 11/03/2015", $tr2->getMessage());
 	}
 
-	/**
-	 * @expectedException        UnexpectedValueException
-	 * @expectedExceptionMessage CSV content invalid
-	 */
 	public function testInvalidSample()
 	{
+		$this->expectedExceptionMessage('CSV content invalid');
+		$this->expectedException(UnexpectedValueException::class);
+
 		$parser = new CsvBnpParibasParser();
 
 		$parser->parse($this->getInvalidSample());
